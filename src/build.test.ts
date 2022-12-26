356acd7dfb3de2da555fa061fdcbe8296c0e59ed
chlbri <bri_lvi@icloud.com>
2022-12-26 13:56:10 +0000

    ci : Test Author: chlbri <bri_lvi@icloud.com>

--- src/build.test.ts
+++ src/build.test.ts
@@ -5,7 +5,7 @@ import { test } from 'vitest';
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
   const exec = process.execPath;
-  const path = join(__dirname, '..', 'lib', 'main.js');
+  const path = join(__dirname, '..', 'build', 'index.js');
   const options: ExecFileSyncOptions = {
     env: {
       INPUT_MILLISECONDS: '500',
