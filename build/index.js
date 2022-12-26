a38c7fc60a169c85a2863273ccae614efe7bf31c
chlbri <bri_lvi@icloud.com>
2022-12-26 17:51:14 +0000

    ci: failed again

--- build/index.js
+++ build/index.js
@@ -6,29 +6,6 @@ require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 
 "use strict";
 
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -39,23 +16,22 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core = __importStar(__nccwpck_require__(686));
+const core_1 = __nccwpck_require__(686);
 const wait_1 = __nccwpck_require__(129);
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const ms = core.getInput('milliseconds');
-            core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-            core.debug(new Date().toTimeString());
+            const ms = (0, core_1.getInput)('milliseconds');
+            (0, core_1.debug)(`Waiting ${ms} milliseconds ...`);
+            (0, core_1.debug)(new Date().toTimeString());
             yield (0, wait_1.wait)(parseInt(ms, 10));
             const time = new Date().toTimeString();
-            core.debug(time);
-            core.setOutput('time', time);
+            (0, core_1.debug)(time);
             // setOutputs({ time });
         }
         catch (error) {
             if (error instanceof Error)
-                core.setFailed(error.message);
+                (0, core_1.setFailed)(error.message);
         }
     });
 }
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,20 @@
-import * as core from '@actions/core';
+import { setFailed, debug, getInput } from '@actions/core';
 import { setOutputs } from './helpers';
 import { wait } from './wait';
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds');
-    core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const ms: string = getInput('milliseconds');
+    debug(`Waiting ${ms} milliseconds ...`);
 
-    core.debug(new Date().toTimeString());
+    debug(new Date().toTimeString());
     await wait(parseInt(ms, 10));
     const time = new Date().toTimeString();
-    core.debug(time);
-    core.setOutput('time', time);
+    debug(time);
 
     // setOutputs({ time });
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message);
+    if (error instanceof Error) setFailed(error.message);
   }
 }
 
